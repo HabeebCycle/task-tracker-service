2a6000190ba76d00cef8062b1869dc69d4c15e6a
Habeeb Okunade <guessy2k2@yahoo.com>
2022-11-30 22:12:09 +1100

    PUT & DELETE operations

--- index.js
+++ index.js
@@ -20,16 +20,34 @@ app.get("/students/:id", (req, res) => {
   const student = students.find((s) => s.id.toString() === studentId); //"3" === 3
 
   if (student) {
-    return res.status(200).json(student);
+    return res.status(200).json(student); //OK
   } else {
-    return res.status(404).json({ message: "Student not found" });
+    return res.status(404).json({ message: "Student not found" }); //Not found
   }
 });
 
 app.post("/students", (req, res) => {
   const body = req.body;
   const newStudentList = [...students, body];
-  return res.status(201).json(newStudentList);
+  return res.status(201).json(newStudentList); //Created
+});
+
+app.put("/students/:id", (req, res) => {
+  const studentId = req.params.id;
+  const student = students.find((s) => s.id.toString() === studentId);
+  if (student) {
+    const body = req.body;
+    const newArray = students.filter((s) => s.id.toString() !== studentId);
+    return res.status(200).json([...newArray, body]);
+  } else {
+    return res.status(400).json({ message: "Student not found" }); //Bad request
+  }
+});
+
+app.delete("/students/:id", (req, res) => {
+  const studentId = req.params.id;
+  const newArray = students.filter((s) => s.id.toString() !== studentId);
+  return res.status(200).json(newArray);
 });
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
